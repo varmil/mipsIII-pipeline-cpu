4745d8ae10926676806fe4f94812a42f4b6f2912
Your Name <fkmks247@gmail.com>
2017-12-07 17:27:23 +0900

    camel to Pascal

--- src/register-file.sv
+++ src/register-file.sv
@@ -1,16 +1,16 @@
 module register_file #(parameter N = 32) (
-  input logic CLK, writeEnable,
-  input logic [$clog2(N) - 1:0] address1, address2, writeAddress,
-  input logic [N - 1:0] writeData,
-  output logic [N - 1:0] readData1, readData2
+  input logic CLK, WriteEnable,
+  input logic [$clog2(N) - 1:0] Address1, Address2, WriteAddress,
+  input logic [N - 1:0] WriteData,
+  output logic [N - 1:0] ReadData1, ReadData2
 );
-  logic [N - 1:0] register [N - 1:0];
+  logic [N - 1:0] Register [N - 1:0];
 
-  assign readData1 = register[address1];
-  assign readData2 = register[address2];
+  assign ReadData1 = Register[Address1];
+  assign ReadData2 = Register[Address2];
 
   always @ (posedge CLK) begin
-    if (writeEnable) register[writeAddress] <= writeData;
+    if (WriteEnable) Register[WriteAddress] <= WriteData;
   end
 
 endmodule // register_file
